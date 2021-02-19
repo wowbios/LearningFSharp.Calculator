4fcab3a8793e38cbaf3feaae7fa0fec8966874f8
Aleksey <wowbios@yandex.ru>
2021-02-20 00:05:31 +0300

    init

---
+++ LearningFSharp.Calculator/LearningFSharp.Calculator/Calc.fs
@@ -0,0 +1,21 @@
+﻿module Calc
+
+open System
+
+let operations =
+  [
+    "+", (+)
+    "-", (-)
+    "*", (*)
+    "/", (/)
+  ] |> Map.ofList
+
+let calc x op y : Result<float, string> =
+  match operations.TryFind op with
+  | Some func -> func (float x) (float y) |> Ok
+  | _ -> sprintf "Invalid operation %s" op |> Error
+  
+let public CalculateExpression(expr: string) : Result<float, string> =
+  match expr.Split(' ') with
+  | x when x.Length = 3 -> calc x.[0] x.[1] x.[2] 
+  | _ -> sprintf "Invalid expression %s" expr |> Error
---
+++ LearningFSharp.Calculator/LearningFSharp.Calculator/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printf "Expression: "
+
+    match Console.ReadLine() |> Calc.CalculateExpression with
+    | Ok value -> sprintf "Result %f" value
+    | Error e -> e
+    |> printfn "%s"
+
+    0 // return an integer exit code
